e91d7d479b87a614e99121d7870595a8f1bcee5c
williamyen0623 <a0988977340@gmail.com>
2022-09-28 15:31:49 +0800

    update

---
+++ pystudy3.py
@@ -0,0 +1,12 @@
+#位元運算子練習
+#show the binary of 19 and 11
+print(bin(19))
+print(bin(11))
+#example
+print(19&11)
+print(19 and 11)
+print(19|11)
+print(19 or 11)
+print(~11)
+print(11>>2)
+print(11<<4)
