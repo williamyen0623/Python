e51376c397cd9577a6305a45b25c88eddceb6d70
williamyen0623 <a0988977340@gmail.com>
2022-09-28 15:12:50 +0800

    import facebook sdk and use put_wall_post

--- pystudy1.py
+++ pystudy1.py
@@ -1,10 +1,11 @@
-import 
-graph = facebook.GraphAPI(access_token='EAAGwg103BWkBAP0n2ZAPK26VbOPZCwO2xnK92tV28TlYf5LWI0IEZBAKYmNhytcbnAtlM93RIzajJsh9YooghXf2RvXJNY70EZCdR2VypZCXa7jD2P19ckslyxdZCLlObllaveZB0tAUgwsRAw26a6yYeqqm6C6JHeMZCTG9fO57dTPDxrUPoZCBZCRVoffhPAF62mqkBznzW2eR4WvBa2K4ZCh8pajg6g31oEPgfjyBoSIvennkwn75ZCnZB',version='3.1')
+import facebook
+graph = facebook.GraphAPI(access_token='EAAGwg103BWkBAP0n2ZAPK26VbOPZCwO2xnK92tV28TlYf5LWI0IEZBAKYmNhytcbnAtlM93RIzajJsh9YooghXf2RvXJNY70EZCdR2VypZCXa7jD2P19ckslyxdZCLlObllaveZB0tAUgwsRAw26a6yYeqqm6C6JHeMZCTG9fO57dTPDxrUPoZCBZCRVoffhPAF62mqkBznzW2eR4WvBa2K4ZCh8pajg6g31oEPgfjyBoSIvennkwn75ZCnZB',version='2.7')
 
-body={
-    'name':"wow",
-    'link':'https://www.youtube.com/watch?v=EqEtLxtL1pU',
-    'caption':'animal',
-    'description':'hahaha'
+# Post something!
+body =  {
+    'name':'Post Test',
+    'caption': 'Check out this example',
+    'description': 'This is a longer description of the attachment',
 }
+graph.put_wall_post(message='Post Test python graphAPI',attachment=body)
 
