a3c21b476027b231a4610d504f10a34e7eee398e
williamyen0623 <a0988977340@gmail.com>
2022-09-28 15:03:47 +0800

    graphapi test

--- pystudy1.py
+++ pystudy1.py
@@ -1,2 +1,10 @@
-a = 1
-print(a)
+import 
+graph = facebook.GraphAPI(access_token='EAAGwg103BWkBAP0n2ZAPK26VbOPZCwO2xnK92tV28TlYf5LWI0IEZBAKYmNhytcbnAtlM93RIzajJsh9YooghXf2RvXJNY70EZCdR2VypZCXa7jD2P19ckslyxdZCLlObllaveZB0tAUgwsRAw26a6yYeqqm6C6JHeMZCTG9fO57dTPDxrUPoZCBZCRVoffhPAF62mqkBznzW2eR4WvBa2K4ZCh8pajg6g31oEPgfjyBoSIvennkwn75ZCnZB',version='3.1')
+
+body={
+    'name':"wow",
+    'link':'https://www.youtube.com/watch?v=EqEtLxtL1pU',
+    'caption':'animal',
+    'description':'hahaha'
+}
+
