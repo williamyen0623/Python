d475492f2303f35c666902a762962f1fab609e36
williamyen0623 <a0988977340@gmail.com>
2022-09-28 15:20:33 +0800

    update

---
+++ pystudy2.py
@@ -0,0 +1,13 @@
+#利用id()檢查變數所存在的記憶體位址是否相同,講述所有變數皆屬於物件相同值的變數存在相同記憶體位址
+x=5
+print(id(x))
+y=5
+print(id(y))
+print(x==y)
+print(x is y)
+print(id(x)==id(y))
+y=20
+print(id(y))
+print(x==y)
+print(x is y)
+print(id(x)==id(y))
