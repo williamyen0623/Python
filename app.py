1a7c3df01db967620b5287d6c08d82caa5ffbfa6
williamyen0623 <a0988977340@gmail.com>
2022-10-03 09:09:11 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route('/')
+@app.route('/data/appInfo/FlaskSE', methods=['GET'])
+@app.route('/data/appInfo/id/5', methods = ['GET'])
+
+def home():
+    return "hello flask"
+
+
+if __name__=="__main__":
+    app.run()
+
+def queryDataMessageByName(FlaskSE):
+    print("type(name) : ", type(FlaskSE))
+    return 'String => {}'.format(FlaskSE)
+
+def queryDateMesageById(id):
+    print("type(id):", type(id))
+    return 'int => {}'.format(id)
