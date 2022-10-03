cda5045f190467b576f84a8f8ba093037ecda596
williamyen0623 <a0988977340@gmail.com>
2022-10-03 09:56:49 +0800

    up

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 @app.route('/data/appInfo/FlaskSE', methods=['GET'])
 @app.route('/data/appInfo/id/5', methods = ['GET'])
 
