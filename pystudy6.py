159ff93ce31804d6dc2e23bf6ccd9f533a7b3d13
williamyen0623 <a0988977340@gmail.com>
2022-10-05 14:25:38 +0800

    update

---
+++ pystudy6.py
@@ -0,0 +1,12 @@
+name = 'Benjamin'
+age = 18
+print(name + ' is ' + str(age) + ' years old.')
+print(name, 'is', age, 'years old. ')
+data = input('Enter a number : ')
+print(data)
+data = int(input('Enter a number : '))
+print(data)
+data = eval(input('Enter a number : '))
+print(data)
+data = eval(input('Enter expression : '))
+print(data)
